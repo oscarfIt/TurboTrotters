8c9d0a800733b91c6f96c08d0f02d86714f6e34e
prashanthPadiachy <prashanth.padiachy@gmail.com>
2025-03-26 14:50:29 +0200

    Added slopCollect Script
    Script rotates the slop in air, destroys slop when the pig collides with it and increases pigs size on collection.

---
+++ Assets/Scripts/slopCollect.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class slopCollect : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float rotationSpeed = 55f;
+    public float scaleIncrease = 0.2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            other.transform.localScale += new Vector3(scaleIncrease, scaleIncrease, scaleIncrease);
+
+            Destroy(gameObject);
+        }
+    }
+}
