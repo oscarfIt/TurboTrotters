8fc483eb9721b8ceebb60dbfe24b24429a58eb1b
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-03-27 20:42:56 +0200

    Add MASS_DECREASE_MULTIPLIER to constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,4 +3,5 @@ public struct Constants
     public const int MAX_MASS = 20;
     public const int MIN_MASS = 5;
     public const int TURBO_BOOST_MULTIPLIER = 20;
+    public const float MASS_DECREASE_MULTIPLIER = 0.001f;
 }
