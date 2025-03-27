29c016443c9c30897e9f0b8bc9fa683f44cf720a
Kyle Fagan <kylefagan5@gmail.com>
2025-03-27 17:16:23 +0200

    Made some script improves and added breakable barrels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-
-    public List<string> playerNames = new List<string>(); // Stores player names
+    public static GameManager Instance { get; private set; }
+    private int playerCount = 2; // Default 2 player
+    private string selectedMap = "Map_1"; // Default map
+    private List<string> availableMaps = new List<string> { "Map_1" };
+    // public List<string> playerNames = new List<string> { "Red", "Blue", "Green", "Yellow" }; // Stores player names
 
     private void Awake()
     {
@@ -21,6 +23,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // public List<string> GetPlayerNames()
+    // {
+    //     return playerNames;
+    // }
+
+    public int GetPlayerCount()
+    {
+        return playerCount;
+    }
+    public void SetPlayerCount(int count)
+    {
+        playerCount = Mathf.Clamp(count, 1, 4); // Ensure it's between 1 and 4
+        Debug.Log("Player Count Set: " + playerCount);
+    }
+
+    public string GetSelectedMap()
+    {
+        return selectedMap;
+    }
+
+    public void SetSelectedMap(string mapName)
+    {
+        selectedMap = mapName;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("CharacterSelection"); // Go to Character Selection
@@ -29,6 +56,7 @@ public class GameManager : MonoBehaviour
     public void StartRace()
     {
         SceneManager.LoadScene("Map_1"); // Load the race track
+        // HUDManager.Instance.SetPlayerNames(playerNames);
     }
 
     public void OpenSettings()
---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager Instance { get; private set; }
+    public List<string> playerNames = new List<string> { "Red", "Blue", "Green", "Yellow" };
+
+    // [SerializeField] private TextMeshProUGUI[] playerPositionTexts;
+    [SerializeField] private TextMeshProUGUI[] playerNameTexts;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        SetPlayerNames(playerNames);
+    }
+
+    // Set Player Names at the Start
+    public void SetPlayerNames(List<string> playerNames)
+    {
+        int count = playerNames.Count;
+        Debug.Log("Player Count: " + count);
+        for (int i = 0; i < playerNameTexts.Length; i++)
+        {
+            if (i < count)
+            {
+                playerNameTexts[i].text = playerNames[i];
+            }
+            else
+            {
+                playerNameTexts[i].text = "";
+            }
+        }
+    }
+
+    // // Update Player Positions
+    // public void UpdatePlayerPositions(string[] positions)
+    // {
+    //     for (int i = 0; i < playerPositionTexts.Length; i++)
+    //     {
+    //         if (i < positions.Length)
+    //         {
+    //             playerPositionTexts[i].text = positions[i];
+    //         }
+    //         else
+    //         {
+    //             playerPositionTexts[i].text = "";
+    //         }
+    //     }
+    // }
+}
