9fae2206afdb625ca9928433201b294d4dad70d9
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-03-27 19:52:56 +0200

    Increase TURBO_BOOST_MULTIPLIER

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,5 +2,5 @@ public struct Constants
 {
     public const int MAX_MASS = 20;
     public const int MIN_MASS = 5;
-    public const int TURBO_BOOST_MULTIPLIER = 2;
+    public const int TURBO_BOOST_MULTIPLIER = 20;
 }
