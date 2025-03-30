b4038682d8510ebe36b169dc67e05790d867b938
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-03-31 01:49:44 +0200

    Finally get this damn plane's orientation correct

--- Assets/Scripts/LeaderTracker.cs
+++ Assets/Scripts/LeaderTracker.cs
@@ -2,25 +2,28 @@ using UnityEngine;
 
 public class LeaderTracker : MonoBehaviour
 {
-
     public GameObject currentLeader;
+    public Vector3 trackCentre;
 
     void Start()
     {
-        
+        trackCentre = GameObject.FindWithTag("TrackCentre").transform.position;
     }
 
     void Update()
     {
-        Debug.Log($"Current Leader: {currentLeader}");
-        Debug.Log($"Position of LeaderTracker = {transform.position}");
+        if (currentLeader == null) return;
+
+        Vector3 toCenter = (trackCentre - transform.position).normalized;
+        // Normal vector to the plane
+        Vector3 normal = Vector3.Cross(toCenter, Vector3.up).normalized;
+        transform.rotation = Quaternion.LookRotation(toCenter, normal);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player entered trigger");
             if (currentLeader != null)
             {
                 transform.SetParent(null);
@@ -31,7 +34,6 @@ public class LeaderTracker : MonoBehaviour
 
             // Reset the object's position relative to the new player
             transform.localPosition = Vector3.zero; // You can modify this to have an offset if needed
-            transform.localRotation = Quaternion.identity; // Optionally reset rotation
         }
     }
 }
