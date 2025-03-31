5358aaa628e35b6a0a480bf624ebc1ce7d455d0c
Oscar Field <89549477+oscarfIt@users.noreply.github.com>
2025-03-31 23:02:41 +0200

    34 position tracking (#37)
    * Add track centre
    
    * Restore script to track leader
    
    * Finally get this damn plane's orientation correct
    
    * Add SetNewLeader() method to LeaderTracker
    
    * Add RaceManager.cs
    
    * Rename PlayerManager to RaceManager and race manager script
    
    * Add TrackSection struct
    
    * Update plane orientation based on track section in LeaderTracker
    
    * Cut some old debugging
    
    * Fix incorrect type in RaceManager
    
    * Fix incorrect leader on overtaking bug
    
    * Add different secant types to TrackSection
    
    * Add TrackCentre and TrackSectionBegin prefabs
    
    * Add track section tags
    
    * Connect RaceManager OnPlayerJoined to PlayerInputManager
    
    * Cut old code
    
    * Add defaults for track centre
    
    * Add pigActions to player

---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RaceManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerJoined(PlayerInput player)
+    {
+        player.gameObject.name = "Player_" + player.playerIndex.ToString();
+    }
+}
---
+++ Assets/Scripts/TrackSection.cs
@@ -0,0 +1,11 @@
+public struct TrackSection
+{
+    public const string NorthStraight = "NorthStraightSection";
+    public const string SouthStraight = "SouthStraightSection";
+    public const string EastStraight = "EastStraightSection";
+    public const string WestStraight = "WestStraightSection";
+    public const string SecantNorthEast = "SecantNorthEastSection";
+    public const string SecantNorthWest = "SecantNorthWestSection";
+    public const string SecantSouthEast = "SecantSouthEastSection";
+    public const string SecantSouthWest = "SecantSouthWestSection";
+}
