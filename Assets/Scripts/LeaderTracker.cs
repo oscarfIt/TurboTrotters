36a1367a2c8208c4e104e584f6fcbec6de810354
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-03-31 21:40:34 +0200

    Update plane orientation based on track section in LeaderTracker

--- Assets/Scripts/LeaderTracker.cs
+++ Assets/Scripts/LeaderTracker.cs
@@ -4,10 +4,11 @@ public class LeaderTracker : MonoBehaviour
 {
     public GameObject currentLeader;
     public Vector3 trackCentre;
-
+    private string currentTrackSection;
     void Start()
     {
         trackCentre = GameObject.FindWithTag("TrackCentre").transform.position;
+        currentTrackSection = TrackSection.SouthStraight;       // Adjust this if we need to start in a different section
     }
 
     void Update()
@@ -24,7 +25,19 @@ public class LeaderTracker : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Vector3 directionToPlayer = (other.transform.position - transform.position);
+            Debug.Log($"Player {other.gameObject.name} entered trigger");
+            Debug.Log($"Direction to player: {directionToPlayer}");
             SetNewLeader(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag(TrackSection.SouthStraight) ||
+                 other.gameObject.CompareTag(TrackSection.EastStraight) ||
+                 other.gameObject.CompareTag(TrackSection.NorthStraight) ||
+                 other.gameObject.CompareTag(TrackSection.WestStraight) ||
+                 other.gameObject.CompareTag(TrackSection.Secant))
+        {
+            Debug.Log($"Plane {gameObject.name} entered track section: {other.gameObject.tag}");
+            currentTrackSection = other.gameObject.tag;
         }
     }
 
@@ -40,5 +53,29 @@ public class LeaderTracker : MonoBehaviour
 
         // Reset the object's position relative to the new player
         transform.localPosition = Vector3.zero; // You can modify this to have an offset if needed
+    }
+    private void UpdatePlaneOrientation()
+    {
+        switch (currentTrackSection)
+        {
+            case TrackSection.SouthStraight:
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+                break;
+            case TrackSection.EastStraight:
+                transform.rotation = Quaternion.Euler(0, -90, -90);
+                break;
+            case TrackSection.NorthStraight:
+                transform.rotation = Quaternion.Euler(0, -180, -90);
+                break;
+            case TrackSection.WestStraight:
+                transform.rotation = Quaternion.Euler(0, -270, -90);
+                break;
+            case TrackSection.Secant:
+                Vector3 toCenter = (trackCentre - transform.position).normalized;
+                // Normal vector to the plane
+                Vector3 normal = Vector3.Cross(toCenter, Vector3.up).normalized;
+                transform.rotation = Quaternion.LookRotation(toCenter, normal);
+                break;
+        }
     }
 }
