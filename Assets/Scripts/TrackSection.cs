f2fc5fa1d88aa252a8cb5370acff6decf2b3defa
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-03-31 21:39:12 +0200

    Add TrackSection struct

---
+++ Assets/Scripts/TrackSection.cs
@@ -0,0 +1,8 @@
+public struct TrackSection
+{
+    public const string NorthStraight = "NorthStraightSection";
+    public const string SouthStraight = "SouthStraightSection";
+    public const string EastStraight = "EastStraightSection";
+    public const string WestStraight = "WestStraightSection";
+    public const string Secant = "SecantSection";
+}
