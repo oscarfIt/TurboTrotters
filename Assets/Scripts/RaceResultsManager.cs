49619d41604cdea3464287a80e26b56316a1eea5
prashanthPadiachy <prashanth.padiachy@gmail.com>
2025-04-01 22:48:17 +0200

    Added results panel. Needs to be called for winner

---
+++ Assets/Scripts/RaceResultsManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class RaceResultsManager : MonoBehaviour
+{
+    public GameObject resultsPanel;
+    public TextMeshProUGUI resultsText;
+    public GameObject returnButton;
+
+    public void ShowResults(int winningPlayerIndex)
+    {
+        resultsPanel.SetActive(true);
+        Time.timeScale = 0f;
+
+        resultsText.text = $"<b><color=gold>Player {winningPlayerIndex + 1} Wins!</color></b>";
+        EventSystem.current.SetSelectedGameObject(returnButton);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // update with your scene name
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
