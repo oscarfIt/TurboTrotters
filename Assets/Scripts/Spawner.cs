3975d9f785c6e21c4c07605ebed22f4751ad14fe
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2025-04-01 18:13:41 +0200

    Add list of spawned GameObects and cleanup method to Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,31 +1,59 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject prefabToSpawn;
+    public List<GameObject> spawnedObjects;
     public int maxToSpawn;
     public float radius = 10f; // Set this in the editor
     private LayerMask groundLayer;
+    private int numToSpawn;
 
     void Start()
     {
         groundLayer = LayerMask.GetMask("Ground");
+        spawnedObjects = new List<GameObject>();
+        numToSpawn = maxToSpawn;
         Spawn();
     }
 
     public void Spawn()
     {
-        int amount = maxToSpawn;
         Vector3 spawnPosition;
-        for (int i = 0; i < amount; i++)
+        for (int i = 0; i < numToSpawn; i++)
         {
             spawnPosition = GetSpawnPosition();
             if (spawnPosition != Vector3.zero)
             {
-                Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+                spawnedObjects.Add(Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity));
                 Debug.Log($"Spawned {prefabToSpawn.name} at {spawnPosition}");
             }
+            else
+            {
+                Debug.LogWarning($"Failed to spawn {prefabToSpawn.name} at random position. Position was zero.");
+            }
         }
+        Debug.Log($"Spawned {numToSpawn} objects of {prefabToSpawn.name}");
+    }
+
+    public void NewLapCleanup()
+    {
+        numToSpawn = 0;
+        Debug.Log($"New lap cleanup for {prefabToSpawn.name}, count = {spawnedObjects.Count}");
+        for (int i = 0; i < spawnedObjects.Count; i++)
+        {
+            if (spawnedObjects[i] == null)
+            {
+                Debug.Log($"Object {i} is null, incrementing objects to spawn.");
+                numToSpawn++;
+            }
+            else
+            {
+                Debug.Log($"Objecct at {i} is {spawnedObjects[i].name}");
+            }
+        }
+        spawnedObjects.RemoveAll(obj => obj == null);
     }
 
     private Vector3 GetSpawnPosition()
